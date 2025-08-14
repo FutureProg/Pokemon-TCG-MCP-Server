04de107756b39f9b752362e87d71690f4003e4ba
Nick Morrison <nmorrison@elementcorp.com>
2025-08-14 15:46:09 -0400

    conversion to hono, using Deno.serve now

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
-import express from 'express';
+import { toReqRes, toFetchResponse } from 'fetch-to-node';
 import { exit } from "node:process";
 import { registerCardTools } from "./src/tools.ts";
 import { registerPaginatedCardTools } from "./src/paginated-tools.ts";
+
+import { Hono } from 'hono';
 
 // Initialize the MCP server
 const server = new McpServer({
@@ -19,8 +21,8 @@ console.log("Starting MCP server");
 const transportType = Deno.env.get("MCP_SERVER_TRANSPORT") ?? 'stdio';
 console.log(`Transport Type: ${transportType}`);
 if (transportType === "http") {
-  const app = express();
-  console.log("Starting express");
+  const app = new Hono();
+  console.log("Starting Hono Server");
   // No middleware - let the transport handle everything
 
   const transport = new StreamableHTTPServerTransport({
@@ -31,15 +33,42 @@ if (transportType === "http") {
   await server.connect(transport);
 
   // Handle all MCP requests (GET, POST, DELETE) at a single endpoint
-  app.all('/mcp', async (req, res) => {
+  app.all('/mcp', async (c) => {
     // Handle the request using the pre-connected transport
-    await transport.handleRequest(req, res);
+    const originalRequest = await c.req.raw;
+    const bodyText = await originalRequest.text();
+
+    // convert to an MCP request
+    const headers = new Headers(originalRequest.headers);
+    const host = headers.get("Host");
+
+    // Remove the port
+    if (host) {
+      headers.set("Host", host.split(":")[0]);
+    }
+
+    if (!headers.get("Origin")) {
+      try {
+        const requestUrl = new URL(originalRequest.url);
+        headers.set("Origin", requestUrl.origin);
+      } catch (error) {
+        console.log("No request origin, falling back to the MCP Server");
+      }
+    }
+    headers.set("Content-Type", "application/json");
+
+    const req = new Request(originalRequest.url, {
+      method: originalRequest.method,
+      headers,
+      body: bodyText
+    });
+    const {req: nodeReq, res: nodeRes} = toReqRes(req);
+
+    await transport.handleRequest(nodeReq, nodeRes);
+    return toFetchResponse(nodeRes);
   });
 
-  app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000/mcp");
-    console.log("Press Ctrl+C to stop the server.");
-  });
+  Deno.serve(app.fetch);
 } else {
   const transport = new StdioServerTransport();
 
